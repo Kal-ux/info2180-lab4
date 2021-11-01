1276a61ee71d838f2a299fe7b0bd4ac21d7389cd
Rashede Callum <rcallum461@gmail.com>
2021-10-31 21:42:28 -0500

    Edited file

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 window.onload = function(){
-   load_results();
+    document.getElementById("button").onclick = function(){
+        show_results(document.getElementById("text_Field").value)
+    }
 }
 
-function load_results() {
-    var xhttp = new XMLHttpRequest();
-    var x = document.getElementById("button");
-    xhttp.open("GET", "http://localhost/info2180-lab4/superheroes.php", true);
-    xhttp.send(null);
-    response = xhttp.responseText
-    console.log(response)
-    x.onclick = function(){
-        alert(xhttp.responseText);  
+function show_results(res) {
+    if (window.XMLHttpRequest) {
+        xhr=new XMLHttpRequest();
+      }
+    
+    xhr.onreadystatechange=function() { 
+        if (this.readyState==4 && this.status==200) {
+          document.getElementById("results").innerHTML=this.responseText;
+
+        }
     }
+    xhr.open("GET","http://localhost/info2180-lab4/superheroes.php?q="+res,true);
+    xhr.send();
 }
